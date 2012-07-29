2e2a20197c97091fc617c3953dbfe12f8d05424a
Jelle Ursem <SchizoDuckie@gmail.com>
2012-07-29 02:44:14 +0200

    added createStatement, updated schema to reflect local test
    Signed-off-by: Jelle Ursem <SchizoDuckie@gmail.com>

--- plain-old-javascript/presentation.js
+++ plain-old-javascript/presentation.js
@@ -2,13 +2,14 @@
 		className: 'Presentation',
 		table : 'presentations',
 		primary : 'ID_Presentation',
-		fields: ['ID_Presentation','ID_Client','name','template','forceUpdate', 'lastUpdated','lastAccessed','ID_Catalog', 'ID_Category'],
+		fields: ['ID_Presentation','ID_Client','name','template','forceUpdate', 'lastUpdated','lastAccessed','ID_Catalog', 'ID_Category', 'globalCSS','globalJS'],
 		relations: {
 			'Slide': dbObject.RELATION_MANY
 		},
 		connectors: {
 			'Slide' : 'Presentationslide'
 		},
+		createStatement: 'CREATE TABLE "presentations" ("ID_Presentation" INTEGER PRIMARY KEY  NOT NULL ,"ID_Client" int(4) NOT NULL ,"name" varchar(256) DEFAULT (NULL) ,"template" varchar(50) NOT NULL ,"forceUpdate" char(1) NOT NULL  DEFAULT ("1") ,"lastUpdated" timestamp NOT NULL  DEFAULT ("0000-00-00 00:00:00") ,"lastAccessed" timestamp NOT NULL  DEFAULT ("0000-00-00 00:00:00") ,"ID_Catalog" INTEGER NOT NULL , "ID_Category" INTEGER, "globalCSS" TEXT, "globalJS" TEXT)',
 		adapter: 'dbAdapter'
 	}, {
 
