f258c3cfecfc35d196f2923ff66b2e5b92110e8a
SchizoDuckie <schizoduckie@gmail.com>
2015-11-07 14:48:28 +0100

    escape from callback hell. Promises ftw <3

--- generate-entity.js
+++ generate-entity.js
@@ -2,9 +2,10 @@ var inquirer = require("inquirer"),
     pluralize = require('pluralize'),
     fs = require('fs'),
     grep = require('simple-grep'),
-    exec = require('child_process').exec
+    exec = require('child_process').exec,
+    Promise = require('es6-promise').Promise;
 
-// file is included here:
+
 eval(fs.readFileSync('src/CRUD.js') + '');
 eval(fs.readFileSync('src/CRUD.SqliteAdapter.js') + '');
 
@@ -39,59 +40,6 @@ console.log([
     "------------------------------------------------"
 ].join("\n"));
 
-var starterQuestions = [{
-    type: "input",
-    name: "entityName",
-    message: "Please name your entity (singular)",
-
-}];
-
-var propertyQuestions = [{
-    type: "input",
-    name: "property",
-    message: "Add a property"
-}, {
-    type: "list",
-    name: "type",
-    message: "What's the type?",
-    choices: [
-        "RELATION",
-        "VARCHAR",
-        "INT",
-        "TINYINT",
-        "SMALLINT",
-        "MEDIUMINT",
-        "BIGINT",
-        "DATE",
-        "DATETIME",
-        "TEXT",
-        "BLOB",
-        "DOUBLE",
-        "FLOAT",
-        "DECIMAL",
-
-    ]
-}];
-
-var noLength = ["DATE", "DATETIME", "TEXT", "BLOB"];
-
-var metaQuestions = [{
-    type: "confirm",
-    name: "createIndex",
-    message: "Create an INDEX on this property?",
-    default: false
-}, {
-    type: "confirm",
-    name: "autoSerialize",
-    message: "Auto-Serialize this property? (Say yes if you plan to put objects or arrays in this property)",
-    default: false
-}, {
-    type: "confirm",
-    name: "askAgain",
-    message: "Want to add another property? ",
-    default: true
-}];
-
 var entity = {
     properties: {}
 };
@@ -100,51 +48,125 @@ var ucFirst = function(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-function askMetaQuestions(property) {
-    inquirer.prompt(metaQuestions, function(results) {
-        entity.properties[property].autoSerialize = results.autoSerialize;
-        entity.properties[property].index = results.createIndex;
+/**
+ * Return aything not promise as a promise similar to angular's $q.when
+ */
+function promiseWrap(something) {
+    return new Promise(function(resolve) {
+        resolve(something);
+    });
+}
 
-        if (results.askAgain) {
-            addProperty();
-        } else {
-            console.log("\nEntity info:");
-            console.log(JSON.stringify(entity, null, "  "));
-        }
+/**
+ * Convert inquirer.prompt to a promise.
+ */
+function promisePrompt(questions) {
+    return new Promise(function(resolve, reject) {
+        inquirer.prompt(questions, function(results) {
+            resolve(results);
+        });
+    });
+}
+
+function askIndex(property) {
+    return promisePrompt({
+        type: "confirm",
+        name: "createIndex",
+        message: "Create an INDEX on this property?",
+        default: false
+    }).then(function(results) {
+        entity.properties[property.property].index = results.createIndex;
+        return property;
     });
 }
 
 function askLength(property) {
-    inquirer.prompt({
-        type: "input",
-        name: "length",
-        message: "Length",
-        default: 1
-    }, function(results) {
-        entity.properties[property].length = results.length;
-        askMetaQuestions(property);
-    });
+    var noLength = ["DATE", "DATETIME", "TEXT", "BLOB"];
+
+    if (noLength.indexOf(property.type) > -1) {
+        return promiseWrap(property);
+    } else {
+        return promisePrompt({
+            type: "input",
+            name: "length",
+            message: "Length",
+            default: 1
+        }).then(function(results) {
+            entity.properties[property.property].length = results.length;
+            return property;
+        });
+    }
 }
 
+function askAutoserialize(property) {
+    var canHoldSerializable = ["TEXT", "BLOB"];
+    if (canHoldSerializable.indexOf(property.type) == -1) {
+        return promiseWrap(property);
+    } else {
+        return promisePrompt({
+            type: "confirm",
+            name: "autoSerialize",
+            message: "Auto-Serialize this property? (Say yes if you plan to put objects or arrays in this property)",
+            default: false
+        }).then(function(results) {
+            entity.properties[property.property].autoSerialize = results.autoSerialize;
+            return property;
+        });
+    }
+}
 
-function addProperty() {
-    inquirer.prompt(propertyQuestions, function(results) {
+function askProperty() {
+    return promisePrompt([{
+        type: "input",
+        name: "property",
+        message: "Enter the name of a new property to add"
+    }, {
+        type: "list",
+        name: "type",
+        message: "What's the type?",
+        choices: "RELATION|VARCHAR|INT|TINYINT|SMALLINT|MEDIUMINT|BIGINT|DATE|DATETIME|TEXT|BLOB|DOUBLE|FLOAT|DECIMAL".split("|")
+    }]).then(function(results) {
         entity.properties[results.property] = {
             type: results.type
         };
-
-        if (noLength.indexOf(results.type) == -1) {
-            askLength(results.property);
-        } else {
-            askMetaQuestions(results.property);
-        }
-
+        return results;
     });
 }
 
-inquirer.prompt(starterQuestions, function(answers) {
+
+function askAnotherProperty() {
+    return promisePrompt({
+        type: "confirm",
+        name: "askAgain",
+        message: "Want to add another property? ",
+        default: true
+    }).then(function(results) {
+        return (results.askAgain) ? addProperty() : entity;
+    });
+}
+
+function outputEntity(entity) {
+    console.log("\nEntity info:");
+    console.log(JSON.stringify(entity, null, "  "));
+    return;
+}
+
+function addProperty() {
+    return askProperty()
+        .then(askLength)
+        .then(askAutoserialize)
+        .then(askIndex)
+        .then(askAnotherProperty);
+}
+
+promisePrompt({
+    type: "input",
+    name: "entityName",
+    message: "Please name your entity (singular)",
+}).then(function(answers) {
     entity.name = ucFirst(answers.entityName);
     entity.table = ucFirst(pluralize.plural(entity.name));
     entity.primary = 'ID_' + entity.name;
-    addProperty();
+    console.log("Creating new CRUD Entity: %s\nTable: %s\nPrimary key: %s", entity.name, entity.table, entity.primary);
+    return addProperty().then(outputEntity);
 });
